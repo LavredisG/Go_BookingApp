c9a2849d2059ec7477afba5d178f5303ecdbdb29
LavredisG <lavredisgoume@gmail.com>
2024-07-30 04:18:48 +0300

    Checkpoint: Arrays & Slices

--- main.go
+++ main.go
@@ -5,19 +5,18 @@ import "fmt"
 func main() {
 	conferenceName := "Go conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
+	var bookings []string
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	//fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
-	// fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
 
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	// ask user for info
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -31,5 +30,11 @@ func main() {
 	fmt.Println("Enter your number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 }
